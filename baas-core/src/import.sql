e5153bf1fd4bc5c96e5ca11061329b478404b2b9
gcatarino <gcatarino@e9b58ad1-6be5-4291-993d-ed4aa7f1b531>
2013-09-25 23:28:43 +0000

    Svn ignore

--- baas-core/src/import.sql
+++ baas-core/src/import.sql
@@ -1,1 +1,1 @@
-insert into user (email, lastname, loggedIn, name, password, username) values ('a@a.com', 'alastname', false, 'aname', 'apassword', 'ausername');
+insert into users (email, lastname, loggedIn, name, password, username) values ('a@a.com', 'alastname', false, 'aname', 'apassword', 'ausername');
--- baas-core/src/uy/com/group05/baascore/common/entities/User.java
+++ baas-core/src/uy/com/group05/baascore/common/entities/User.java
@@ -5,10 +5,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @Entity
 @XmlRootElement
+@Table(name = "USERS")
 public class User {
 	
 	@Id
