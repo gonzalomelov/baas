af68e1fce0ff4ac3502d50184e4ef26048af9b08
gcatarino <gcatarino@e9b58ad1-6be5-4291-993d-ed4aa7f1b531>
2013-10-14 20:29:16 +0000

    Cambio web.xml

--- baas-core/generated-entities/uy/com/group05/baascore/common/entities/Application_.java
+++ baas-core/generated-entities/uy/com/group05/baascore/common/entities/Application_.java
@@ -1,5 +1,6 @@
 package uy.com.group05.baascore.common.entities;
 
+import java.util.UUID;
 import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
@@ -9,12 +10,14 @@ public abstract class Application_ {
 
 	public static volatile SingularAttribute<Application, Long> id;
 	public static volatile ListAttribute<Application, User> users;
+	public static volatile ListAttribute<Application, ExternalClient> externalClients;
+	public static volatile ListAttribute<Application, ExternalApplication> externalApplications;
 	public static volatile ListAttribute<Application, Role> roles;
 	public static volatile ListAttribute<Application, PushChannel> pushChannels;
 	public static volatile SingularAttribute<Application, String> name;
-	public static volatile SingularAttribute<Application, String> apiClientId;
+	public static volatile SingularAttribute<Application, UUID> apiClientId;
 	public static volatile ListAttribute<Application, Entity> entities;
-	public static volatile SingularAttribute<Application, String> apiClientSecret;
+	public static volatile ListAttribute<Application, Role> externalClientsRoles;
 	public static volatile ListAttribute<Application, Client> clients;
 
 }
--- baas-core/generated-entities/uy/com/group05/baascore/common/entities/Client_.java
+++ baas-core/generated-entities/uy/com/group05/baascore/common/entities/Client_.java
@@ -1,5 +1,6 @@
 package uy.com.group05.baascore.common.entities;
 
+import java.util.UUID;
 import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
@@ -9,14 +10,14 @@ public abstract class Client_ {
 
 	public static volatile SingularAttribute<Client, Long> id;
 	public static volatile SingularAttribute<Client, Application> application;
-	public static volatile SingularAttribute<Client, String> accessToken;
+	public static volatile SingularAttribute<Client, UUID> accessToken;
 	public static volatile SingularAttribute<Client, String> email;
 	public static volatile ListAttribute<Client, Role> roles;
 	public static volatile ListAttribute<Client, PushChannel> pushChannels;
 	public static volatile SingularAttribute<Client, String> name;
 	public static volatile SingularAttribute<Client, String> lastname;
 	public static volatile SingularAttribute<Client, Boolean> loggedIn;
-	public static volatile SingularAttribute<Client, String> refreshToken;
+	public static volatile SingularAttribute<Client, UUID> refreshToken;
 	public static volatile SingularAttribute<Client, String> password;
 
 }
