2b48eb2d9801fc7f4cfeb60b64b3eec8484456da
gcatarino <gcatarino@e9b58ad1-6be5-4291-993d-ed4aa7f1b531>
2013-10-14 20:41:14 +0000

    Cambio url login facebook

--- baas-admin/src/uy/com/group05/baasadmin/pl/beans/UserLoginBean.java
+++ baas-admin/src/uy/com/group05/baasadmin/pl/beans/UserLoginBean.java
@@ -108,4 +108,14 @@ public class UserLoginBean {
 		
 		return "/index.xhtml?faces-redirect=true";
 	}
+	
+	public String getFacebookUrlAuth() {
+        
+        String appId = "641457435904811";
+        String redirectUrl = "http://localhost:8080/baas-admin/index.sec";
+        String returnValue = "https://www.facebook.com/dialog/oauth?client_id="
+                + appId + "&redirect_uri=" + redirectUrl
+                + "&scope=email";
+        return returnValue;
+    }
 }
