c665473772c2025a02d58c5fb523b3fd810f7a7f
gcatarino <gcatarino@e9b58ad1-6be5-4291-993d-ed4aa7f1b531>
2013-11-28 07:11:31 +0000

    estilos y Placeholder

---
+++ baas-admin/WebContent/js/Placeholder.js
@@ -0,0 +1,12 @@
+$(function() {
+	$(".emailPlaceHolder").attr("placeholder","Email");	
+	
+	$(".passwordPlaceHolder").attr("placeholder","Password");
+	
+	$(".namePlaceHolder").attr("placeholder","Name");
+	
+	$(".lastNamePlaceHolder").attr("placeholder","Last Name");
+	
+	$(".repasswordPlaceHolder").attr("placeholder","Repeat Password");
+	
+});
